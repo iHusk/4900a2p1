5d6bcf8ab8626b73e0aa7d2a439bac37b12774e3
iHusk <tjhayes@unomaha.edu>
2022-02-21 16:31:02 -0600

    again

--- movie/api/views.py
+++ movie/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import *
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
